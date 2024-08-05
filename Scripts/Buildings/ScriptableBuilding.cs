6a0f9e31f13b1cf83ac2298aba567e83d2d4f9ba
Jachym Fukal <168704319+Projachmann@users.noreply.github.com>
2024-08-05 09:37:39 +0200

    Delete Scripts/Buildings directory

--- Scripts/Buildings/ScriptableBuilding.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Building", menuName = "Scriptable Building")]
-public class ScriptableBuilding : ScriptableObject
-{
-    public BaseBuilding buildingPrefab;
-}
